f2c29b529ecb0e56a0ce7ab6ae3f56b15b3312e5
Rameez <rameezkhan.1@hotmail.co.uk>
2017-07-19 06:03:23 +0100

    Array Introduction C++

---
+++ C++/HackerRank/learn-cpp/arrays-introduction/arrays-introduction.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i=1; i<=n; i++) cin>>arr[i];
+    for (int j=n; j>=1; j--) cout<<arr[j]<<" ";
+
+    return 0;
+}
