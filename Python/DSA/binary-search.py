00718a173736d41e461e7c3bd5490bf4434e7738
rxmeez <rameezkhan.1@hotmail.co.uk>
2017-11-01 16:32:12 +0000

    binary search in python

---
+++ Python/DSA/binary-search.py
@@ -0,0 +1,17 @@
+# Binary Search
+
+
+def binarySearch(input_array, value):
+    low = 0
+    high = len(input_array) - 1
+
+    while (low <= high):
+        mid = (low + high) // 2
+        if (input_array[mid] == value):
+            return mid
+        elif (input_array[mid] < value):
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
